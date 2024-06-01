7b0eef0380af00053123c5d5a8b078f291ae12f9
BradJKim <bradjkim03@gmail.com>
2024-05-31 18:16:07 -0700

    component and model setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Task from "./components/task/task.js";
+
+const content = document.querySelector("#content");
+
+const taskEx = Task();
+
+console.log(taskEx);
---
+++ src/models/task.js
@@ -0,0 +1,8 @@
+class Task {
+    /* const id = 1; // self asign
+    let title = "New Task";
+    let description = "";
+    let dueDate = ""; // date-fns
+    let priority = 0; // int 1-5
+    let checked = false; */
+}
