7d88ca1758705b77bbeb079e277219a4346e2c10
BradJKim <bradjkim03@gmail.com>
2024-05-31 21:54:03 -0700

    added css loader

--- src/components/task/taskComponent.js
+++ src/components/task/taskComponent.js
@@ -1,7 +1,16 @@
 import Task from "../../models/task.js";
+import "./style.css";
 
 export default function TaskComponent() {
     const task = new Task();
+    const element = document.createElement('div');
+    element.className="taskComponent";
 
-    return "nothing";
+    //TODO: handling html component using data from task
+
+    // Simple box with 3 dots
+    // expand with description and other info
+    // click x to get rid of it
+
+    return element;
 }
