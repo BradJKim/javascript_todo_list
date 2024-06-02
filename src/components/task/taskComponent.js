c8478d7d2a5a85a4c19d1a1d7f08cfe8bd0c4dba
BradJKim <bradjkim03@gmail.com>
2024-06-01 20:23:04 -0700

    implemented simple task extension functionality

--- src/components/task/taskComponent.js
+++ src/components/task/taskComponent.js
@@ -2,8 +2,7 @@ import Task from "../../models/task.js";
 import TaskLoader from "../../../utils/taskLoader/taskLoader.js";
 import "./style.css";
 
-export default function TaskComponent(projectID, taskObject) {
-    const parentProjectID = projectID;
+export default function TaskComponent(taskObject) {
     const task = taskObject != null ? taskObject : new Task();
     const element = document.createElement("div");
     element.className = "task";
@@ -14,21 +13,55 @@ export default function TaskComponent(projectID, taskObject) {
     // expand with description and other info on top of dom
     // click x to get rid of it
 
-    const checkBox = document.createElement("input");
+    // function for enabling visibility of task extention
+    function toggleExtension() {
+        const taskExtension = element.children[0];
+        taskExtension.classList.toggle("show");
+    }
+
+    // hidden task extension for task details
+    const taskExtension = document.createElement("div");
+    taskExtension.className = "taskExtension";
+
+    const exitButton = document.createElement("button");
+    exitButton.textContent = "exit";
+    exitButton.addEventListener('click', (e) => {toggleExtension()})
+
+    taskExtension.appendChild(exitButton);
+
+    // description in hidden task extension
+    const dueDate = document.createElement("p");
+    dueDate.textContent = "Due: 10/10/25";
+
+    taskExtension.appendChild(dueDate);
+
+    element.append(taskExtension);
+
+    // description in hidden task extension
+    const description = document.createElement("p");
+    description.textContent = "apple cider";
+
+    taskExtension.appendChild(description);
+
+    element.append(taskExtension);
+
+    // Checkbox to access check property
+    const checkBox = document.createElement("input"); // make green if checked
     checkBox.setAttribute("type", "checkbox");
     element.append(checkBox);
 
+    // Displayed Title of task
     const titleHeader = document.createElement("h3");
     titleHeader.innerHTML = task.getTitle;
     element.append(titleHeader);
 
-    const menuButton = document.createElement("button");
-    menuButton.innerHTML = "details";
-    element.append(menuButton);
-
+    // Extension button to enable task extention
+    const extensionButton = document.createElement("button");
+    extensionButton.innerHTML = "details";
+    extensionButton.className = "menuButton";
+    extensionButton.addEventListener("click", (e) => {toggleExtension()});
     
-
-    
+    element.append(extensionButton);
 
     return element;
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import TaskComponent from "./components/task/taskComponent.js";
 
 const content = document.querySelector("#content");
 
-const taskEx = TaskComponent("123");
-
-console.log(taskEx);
-
+const taskEx = TaskComponent();
 content.appendChild(taskEx);
+
+
